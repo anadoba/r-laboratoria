028cab86a0d2d056a91881241cfad306f5c0598d
Adam Nadoba <anadoba@windowslive.com>
2016-11-02 11:11:34 +0100

    Zadanie 1 - kod gotowy

--- Zadanie01/Kod01.r
+++ Zadanie01/Kod01.r
@@ -4,4 +4,20 @@
 
 setwd("C:/r-laboratoria/Zadanie01")
 
-dane <- read.csv(file="Dane01.csv", head=TRUE)
+spolki <- read.csv(file="Dane01.csv", head=TRUE)
+
+#Wielkoœæ spó³ek
+wielkoscKwartyle <- quantile(spolki[,2], type=6)
+#Q1 = kwartyle[2] itd
+wielkoscQ1 <- wielkoscKwartyle[2]
+wielkoscQ2 <- wielkoscKwartyle[3]
+wielkoscQ3 <- wielkoscKwartyle[4]
+
+#dplyr przykrywa standardowe 'filter'
+library(dplyr)
+
+spolkiBardzoMale <- filter(spolki, spolki[2] <= wielkoscQ1)
+spolkiMale <- filter(spolki, wielkoscQ1 < spolki[2] & spolki[2] <= wielkoscQ2)
+spolkiSrednie <- filter(spolki, wielkoscQ2 < spolki[2] & spolki[2] <= wielkoscQ3)
+spolkiDuze <- filter(spolki, wielkoscQ3 < spolki[2] & spolki[2])
+
