2c3fcba68e23f49e1c39ebca1346d53042ee2f64
Adam Nadoba <anadoba@windowslive.com>
2016-11-03 19:08:17 +0100

    dokoncz zadanie 1

--- Zadanie01/Kod01.r
+++ Zadanie01/Kod01.r
@@ -19,5 +19,20 @@ library(dplyr)
 spolkiBardzoMale <- filter(spolki, spolki[2] <= wielkoscQ1)
 spolkiMale <- filter(spolki, wielkoscQ1 < spolki[2] & spolki[2] <= wielkoscQ2)
 spolkiSrednie <- filter(spolki, wielkoscQ2 < spolki[2] & spolki[2] <= wielkoscQ3)
-spolkiDuze <- filter(spolki, wielkoscQ3 < spolki[2] & spolki[2])
+spolkiDuze <- filter(spolki, wielkoscQ3 < spolki[2])
 
+sredniaCzSpolkiBardzoMale <- mean(spolkiBardzoMale[,3])
+medianaCzSpolkiBardzoMale <- median(spolkiBardzoMale[,3])
+odchylenieCzSpolkiBardzoMale <- sqrt(sum((spolkiBardzoMale[,3] - sredniaCzSpolkiBardzoMale)^2)/nrow(spolkiBardzoMale))
+
+sredniaCzSpolkiMale <- mean(spolkiMale[,3])
+medianaCzSpolkiMale <- median(spolkiMale[,3])
+odchylenieCzSpolkiMale <- sqrt(sum((spolkiMale[,3] - sredniaCzSpolkiMale)^2)/nrow(spolkiMale))
+
+sredniaCzSpolkiSrednie <- mean(spolkiSrednie[,3])
+medianaCzSpolkiSrednie <- median(spolkiSrednie[,3])
+odchylenieCzSpolkiSrednie <- sqrt(sum((spolkiSrednie[,3] - sredniaCzSpolkiSrednie)^2)/nrow(spolkiSrednie))
+
+sredniaCzSpolkiDuze <- mean(spolkiDuze[,3])
+medianaCzSpolkiDuze <- median(spolkiDuze[,3])
+odchylenieCzSpolkiDuze <- sqrt(sum((spolkiDuze[,3] - sredniaCzSpolkiDuze)^2)/nrow(spolkiDuze))
